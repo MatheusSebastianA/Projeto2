30d8fd95f3c542fc6289a7b347a4d97866ee1000
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-04-11 10:46:54 -0300

    Update lib_lista_chave.h
    Agora "chave" é apenas um caracter, e não mais uma string

--- lib_lista_chave.h
+++ lib_lista_chave.h
@@ -1,7 +1,7 @@
 #include "lib_lista_posicao.h"
 /* Nodo que apresenta uma chave, um ponteiro para o próximo nodo e uma lista de valores com as posições */
 struct nodo_l_chave {
-    char *chave;
+    char chave;
     struct nodo_l_chave *prox;
     lista_p_t *lista_pos;
 };
@@ -21,12 +21,12 @@ int vazia_lista_chave(lista_c_t *l);
 
 lista_c_t *destroi_lista_chave (lista_c_t *l);
 
-void copia_chave(char *chave, char *chave_copia);
+void copia_chave(char chave, char chave_copia);
 
 int existe_chave(lista_c_t *l, char *chave);
 
-int insere_ini_lista_chave(lista_c_t *l, char *chave, char *pos);
+int insere_ini_lista_chave(lista_c_t *l, char chave, char *pos);
 
-int insere_ordem_lista_chave (lista_c_t *l, char *chave, char *pos);
+int insere_ordem_lista_chave (lista_c_t *l, char chave, char *pos);
 
 void imprime_lista_chave(lista_c_t *l);
