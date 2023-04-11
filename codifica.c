643974db1af416f952c76e827bea719c62e94690
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-04-11 11:37:59 -0300

    Update codifica.c
    Aparentemente está codificando corretamente.

--- codifica.c
+++ codifica.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "lib_lista_chave.h"
-#define stringingsize 256
 
-char *codifica_string(lista_c_t *l, char *string, char *string_codificada){
-    int i, tam;
-    char *chave;
+int codifica_caractere(lista_c_t *l, char c, char *c_cod){
+    int cont, i;
+    nodo_lc_t *aux;
+    aux = l->ini;
 
-    char = malloc(sizeof(char)*2);
-
-    tam = strlen(string);
-    for(i = 0; i < tam; i++){
-        *chave = *string[0];
-        if (existe_chave(l, chave)){
-            
-        }
+    if (c == 32){
+        c_cod[0] == '-';
+        c_cod[1] == '1';
+        c_cod[2] == '\0';
+        return 1;
     }
 
-    return string_codificada;
+    if (existe_chave(l, &c))
+        while (aux != NULL){
+        if (aux->chave == c){
+            if (aux->lista_pos->tamanho > 1){
+                cont = rand() % (aux->lista_pos->tamanho - 1);
+                for (i = 0; i < cont; i++)
+                    aux->lista_pos->ini = aux->lista_pos->ini->prox;
+            }
+            
+            for (i = 0; i < strlen(aux->lista_pos->ini->pos); i++)
+                c_cod[i] = aux->lista_pos->ini->pos[i];
+            c_cod[i+1] = '\0';
+            return 1;
+        }
+        aux = aux->prox;
+    }
+
+    return 0;
 }
