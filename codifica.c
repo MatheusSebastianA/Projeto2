78d379abd44836baf9e1202d012383c5122f6a93
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-04-14 11:48:56 -0300

    Update codifica.c

--- codifica.c
+++ codifica.c
@@ -1,37 +1,56 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include "lib_lista_chave.h"
-
 int codifica_caractere(lista_c_t *l, char c, char *c_cod){
-    int cont, i;
+    int cont, i, tam;
     nodo_lc_t *aux;
+    nodo_lp_t *aux_ini_pos;
     aux = l->ini;
 
-    if (c == 32){
+    
+    if (c == ' '){
         c_cod[0] = '-';
         c_cod[1] = '1';
         c_cod[2] = '\0';
         return 1;
     }
 
-    if (existe_chave(l, &c))
+    if (existe_chave(l, &c)){
         while (aux != NULL){
-        if (aux->chave == c){
-            if (aux->lista_pos->tamanho > 1){
-                cont = rand() % (aux->lista_pos->tamanho - 1);
-                for (i = 0; i < cont; i++)
-                    aux->lista_pos->ini = aux->lista_pos->ini->prox;
+            if (aux->chave == c){
+                cont = aux->lista_pos->tamanho;
+
+                if(cont == 1){
+                    aux_ini_pos = aux->lista_pos->ini;
+                    tam = strlen(aux_ini_pos->pos);
+                }
+
+                if (cont > 1){
+                    cont = rand() % cont;
+                    aux_ini_pos = aux->lista_pos->ini;
+                    for (i = 0; i < cont; i++){                   
+                        if(aux_ini_pos->prox != NULL){
+                            aux_ini_pos = aux_ini_pos->prox;
+                        }
+                    }
+                    tam = strlen(aux_ini_pos->pos);
+                }
+                
+
+                if (tam == 1){
+                    c_cod[0] = aux_ini_pos->pos[0];
+                    c_cod[1] = '\0';
+                }
+
+                for (i = 0; i < tam; i++)
+                    c_cod[i] = aux_ini_pos->pos[i];
+                c_cod[i+1] = '\0';
+                return 1;
             }
-            
-            for (i = 0; i < strlen(aux->lista_pos->ini->pos); i++)
-                c_cod[i] = aux->lista_pos->ini->pos[i];
-            c_cod[i+1] = '\0';
-            return 1;
-        }
         aux = aux->prox;
+        }
     }
+
+    c_cod[0] = 'N';
+    c_cod[1] = 'F';
+    c_cod[2] = '\0';
 
     return 0;
 }
