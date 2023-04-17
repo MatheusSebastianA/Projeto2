f3cbbd0d106e0d966a00bb8af408b1fc2f6797fc
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-04-17 14:33:28 -0300

    Add files via upload

---
+++ beale.c
@@ -0,0 +1,93 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lib_lista_chave.h"
+#include "lib_lista_posicao.h"
+#include "codifica.h"
+#include "decodifica.h"
+#include <time.h>
+
+int main(){
+    srand(time(NULL));
+    int cont_posicao;
+    char *pos, *palavra, primeira_letra, c, *letra_cod, letra_decod, *num_pos;
+    FILE *arq, *arq_frase, *arq_dst, *arq_decod;
+    lista_c_t *lista_chave;
+
+    lista_chave = cria_lista_chave();
+
+    pos = malloc(sizeof(char)*256);
+    palavra = malloc(sizeof(char)*256);
+    letra_cod = malloc(sizeof(char)*15);
+    num_pos = malloc(sizeof(char)*256);
+    cont_posicao = 0;
+
+    arq = fopen("livro_cifras.txt", "r");
+
+    if ( ! arq ){
+        printf("Erro ao abrir arquivo");
+        return 0;
+    }
+
+    while (fscanf (arq, "%s", palavra) !=  EOF){
+        primeira_letra = *palavra+0;
+        sprintf(pos, "%d", cont_posicao);
+        cont_posicao++;
+        insere_ordem_lista_chave(lista_chave, primeira_letra, pos);
+    }
+    
+    imprime_lista_chave(lista_chave);
+
+    arq_frase = fopen("frase.txt", "r");
+    
+    if ( ! arq_frase ){
+        printf("Erro ao abrir arquivo");
+        return 0;
+    }
+
+    
+    arq_dst = fopen("frase_cod.txt", "w");
+
+    if ( ! arq_dst ){
+        printf("Erro ao abrir arquivo");
+        return 0;
+    }
+
+    c = fgetc(arq_frase);
+    while ( c != EOF){  
+        if (c != '\n'){
+            codifica_caractere(lista_chave, c, letra_cod);
+            fprintf(arq_dst, "%s ", letra_cod);
+        }
+        c = fgetc(arq_frase);
+    }
+
+    fclose(arq_dst);
+
+    if (!(arq_decod = fopen("frase_deco.txt", "w"))){
+        printf("Erro ao abrir arquivo");
+        return 0;
+    }
+
+    if (!(arq_dst = fopen("frase_cod.txt", "r")) ){
+        printf("Erro ao abrir arquivo");
+        return 0;
+    }
+
+    while(fscanf(arq_dst, "%s", num_pos) != EOF){
+        decodifica_caractere(lista_chave, num_pos, &letra_decod);
+        fprintf(arq_decod, "%c", letra_decod);
+    }
+
+    
+    fclose(arq_frase);
+    fclose(arq);
+    fclose(arq_decod);
+    fclose(arq_dst);
+    destroi_lista_chave(lista_chave);
+    free(letra_cod);
+    free(pos);
+    free(palavra);
+    free(num_pos);
+    return 0;
+}  
---
+++ makefile
@@ -0,0 +1,33 @@
+# Makefile de exemplo (Manual do GNU Make)
+     
+CFLAGS = -Wall -std=c99 -g  # flags de compilacao
+LDFLAGS = -lm
+
+CC = gcc
+
+# arquivos-objeto
+	objects = beale.o lib_lista_chave.o  lib_lista_posicao.o codifica.o decodifica.o 
+     
+beale: beale.o lib_lista_chave.o  lib_lista_posicao.o codifica.o decodifica.o 
+	$(CC) -o beale beale.o lib_lista_chave.o  lib_lista_posicao.o codifica.o decodifica.o $(LDFLAGS)
+
+lib_lista_chave.o: lib_lista_chave.c
+	$(CC) -c $(CFLAGS) lib_lista_chave.c
+
+lib_lista_posicao.o: lib_lista_posicao.c
+	$(CC) -c $(CFLAGS)  lib_lista_posicao.c
+
+codifica.o: codifica.c
+	$(CC) -c $(CFLAGS) codifica.c
+
+decodifica.o: decodifica.c
+	$(CC) -c $(CFLAGS) decodifica.c
+
+beale.o: beale.c
+	$(CC) -c $(CFLAGS) beale.c
+
+clean:
+	rm -f $(objects) mundo
+
+
+
