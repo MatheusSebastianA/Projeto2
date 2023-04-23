475b686beb8774ddd50b5f0cc088c3bc6d15bfbd
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-04-23 12:14:11 -0300

    Remoção de comentários

--- codifica.h
+++ codifica.h
@@ -2,8 +2,6 @@
 #define codifica
 #include "lib_lista_chave.h"
 
-/*Função que faz a codificação de uma chave passada para uma posição*/
-
 int codifica_caractere(lista_c_t *l, char c, char *c_cod);  
 
 void codifica_arquivo(lista_c_t *l, FILE *mensagem_original, FILE *mensagem_codificada);
