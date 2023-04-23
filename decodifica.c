34a8bfc8612aed0fa88d667ff68b60640e52e3b8
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-04-23 12:14:41 -0300

    Comentado e retirado uma função e colocado na lib de posições

--- decodifica.c
+++ decodifica.c
@@ -3,40 +3,17 @@
 #include <string.h>
 #include "decodifica.h"
 
-int posicao_existe_lista_pos(lista_p_t *l_p, char *chave_pos){
-    int i, igual, tam;
-    nodo_lp_t *aux;
-
-    aux = l_p->ini;
-
-    while (aux != NULL){
-        tam = strlen(chave_pos);
-        igual = 0;
-
-        if (tam == strlen(aux->pos)){
-            for(i = 0; i < tam; i++){
-                if(chave_pos[i] == aux->pos[i])
-                    igual++;
-            }
-            if(igual == tam){
-                return 1;
-            }
-        }
-       aux = aux->prox;
-    }
-
-    return 0;
-}
-
+/*  Decodifica uma chave passada para uma posição
+    Insere o caracter que representa o código decodificado se funcionou ou "-" se não encontrou a posição em c_decod*/
 void decodifica_caractere(lista_c_t *l, char *chave_pos, char *c_decod){
     nodo_lc_t *aux_chave;
 
-    if (chave_pos[0] == '-' && chave_pos[1] == '1'){
+    if (chave_pos[0] == '-' && chave_pos[1] == '1'){ /* Se for encontrado -1 retorna ' '*/
         *c_decod = ' ';
         return;
     }
 
-    if (chave_pos[0] == '-' && chave_pos[1] == '2'){
+    if (chave_pos[0] == '-' && chave_pos[1] == '2'){ /* Se for encontrado -2 retorna '\n' */
         *c_decod = '\n';
         return;
     }
@@ -44,27 +21,29 @@ void decodifica_caractere(lista_c_t *l, char *chave_pos, char *c_decod){
     aux_chave = l->ini;
 
     while(aux_chave != NULL){ /*Laço que passa por todas as chaves*/
-        if (posicao_existe_lista_pos(aux_chave->lista_pos, chave_pos)){ /*Para cada lista de posições dessa chave, chama uma função para verificar se existe alguma posição que representa o código procurado*/ 
+        if (posicao_existe_lista_pos(aux_chave->lista_pos, chave_pos)){ /* Para cada lista de posições dessa chave, chama uma função para verificar se existe alguma posição que representa o código procurado*/ 
             *c_decod = aux_chave->chave;
             return;
         }
         aux_chave = aux_chave->prox;
     }
 
-    *c_decod = '-'; /*Caso não tenha sido encontrado a posição é retornado "-" para representar "Not Found"*/
+    *c_decod = '-'; /* Caso não tenha sido encontrado a posição é retornado "-" para representar que não foi encontrado (valor -3 por exemplo) */
 
     return;
 }
 
+/*  Decodifica um arquivo com uma mensagem codificada e insere em outro arquivo a mensagem codificada.
+    Quem chamou a função deve abrir e fechar o arquivo. */
 void decodifica_arquivo(lista_c_t *l, FILE *mensagem_codificada, FILE *mensagem_decodificada){
     char *num_pos, letra_decod;
 
     if(!(num_pos = malloc(sizeof(char)*256)))
         return;
 
-    while(fscanf(mensagem_codificada, "%s", num_pos) != EOF){
-        decodifica_caractere(l, num_pos, &letra_decod);
-        fprintf(mensagem_decodificada, "%c", letra_decod);
+    while(fscanf(mensagem_codificada, "%s", num_pos) != EOF){ /* Enquanto tiver uma palavra, será decodificado a string lida do arquivo */
+        decodifica_caractere(l, num_pos, &letra_decod); 
+        fprintf(mensagem_decodificada, "%c", letra_decod); /* Insere valor decodificado no arquivo de mensagem decodificada */
     }
 
     free(num_pos);
