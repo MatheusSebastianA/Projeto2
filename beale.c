324c1976a9ac3afa6d330b34dd940e1be1436ee1
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-04-27 20:40:49 -0300

    Update beale.c

--- beale.c
+++ beale.c
@@ -8,43 +8,11 @@
 #include "lib_lista_posicao.h"
 #include "codifica.h"
 #include "decodifica.h"
-
-int arquivo_existe(char *arg){
-    FILE *arquivo;
-
-    if ((arquivo = fopen(arg, "r"))){
-        printf("O arquivo %s já existe e não será editado\n", arg);
-        if(arquivo != NULL)
-            fclose(arquivo);
-        return 1;
-    }
-
-    return 0;
-}
-
-FILE* abre_leitura_arquivo(char* arg){
-    FILE* arquivo = fopen(arg, "r"); 
-    if (arquivo == NULL) {
-        printf("Não foi possível abrir o arquivo %s.\n", arg);
-        exit(1);
-    }
-
-    return arquivo;
-}
-
-FILE* abre_escrita_arquivo(char* arg){
-    FILE* arquivo = fopen(arg, "w"); 
-    if (arquivo == NULL) {
-        printf("Não foi possível abrir o arquivo %s \n", arg);
-        exit(1);
-    }
-
-    return arquivo;
-}
+#include "lib_trata_arquivo.h"
 
 int main(int argc, char **argv){
     srand(time(NULL));
-    int option, casoED;
+    int option, casoE = 0, casoD = 0;
     char *argB = NULL, *argM = NULL, *argO = NULL, *argC = NULL, *argI = NULL;
     FILE *LivroCifra = NULL, *MensagemOriginal = NULL, *ArquivoDeChaves = NULL, *MensagemDecodificada = NULL, *MensagemCodificada = NULL;
     lista_c_t *lista_chave;
@@ -53,10 +21,10 @@ int main(int argc, char **argv){
     while ((option = getopt (argc, argv, "edb:m:o:c:i:")) != -1)
         switch (option){
             case 'e':
-                casoED = 0;
+                casoE += 1;
                 break;
             case 'd':
-                casoED = 1;
+                casoD += 1;
                 break;
             case 'b':
                 argB = optarg;
@@ -78,37 +46,41 @@ int main(int argc, char **argv){
 	            return (1);
       }   
 
-    if(casoED == 0){
-        if(arquivo_existe(argC) || arquivo_existe(argO))
-            return 1;
-        
-        LivroCifra = abre_leitura_arquivo(argB);
-        lista_chave = cria_lista_chave();
+    if(casoE + casoD != 1){
+        printf("A quantidade de argumentos '-e' ou '-d' é inválida\n");
+        return 1;
+    }
 
-        valores_livro_cifra(lista_chave, LivroCifra);
-        ArquivoDeChaves = abre_escrita_arquivo(argC);
-        imprime_lista_chave_arq(lista_chave, ArquivoDeChaves);
+    if(casoE == 1){
+
+        LivroCifra = abre_leitura_arquivo(argB);
         MensagemOriginal = abre_leitura_arquivo(argM);
         MensagemCodificada = abre_escrita_arquivo(argO);
+
+        lista_chave = cria_lista_chave();
+        valores_livro_cifra(lista_chave, LivroCifra);
         codifica_arquivo(lista_chave, MensagemOriginal, MensagemCodificada);
 
+        if(!(arquivo_existe(argC))){
+            ArquivoDeChaves = abre_escrita_arquivo(argC);
+            imprime_lista_chave_arq(lista_chave, ArquivoDeChaves);
+            fclose(ArquivoDeChaves);
+        }
+
         fclose(LivroCifra);
-        fclose(ArquivoDeChaves);
         fclose(MensagemOriginal);
         fclose(MensagemCodificada);
     }
     
     else{
         if(argC != NULL){
-            if(arquivo_existe(argO))
-                return 1;
 
             ArquivoDeChaves = abre_leitura_arquivo(argC);
-            lista_chave = cria_lista_chave();
-
-            valores_arquivo_chaves(lista_chave, ArquivoDeChaves);
             MensagemCodificada = abre_leitura_arquivo(argI);
             MensagemDecodificada = abre_escrita_arquivo(argO);
+
+            lista_chave = cria_lista_chave();
+            valores_arquivo_chaves(lista_chave, ArquivoDeChaves);
             decodifica_arquivo(lista_chave, MensagemCodificada, MensagemDecodificada);
         
             fclose(ArquivoDeChaves);
@@ -121,11 +93,11 @@ int main(int argc, char **argv){
                 return 1;
 
             LivroCifra = abre_leitura_arquivo(argB);
-            lista_chave = cria_lista_chave();
-            
-            valores_livro_cifra(lista_chave, LivroCifra);
             MensagemCodificada = abre_leitura_arquivo(argI);
             MensagemDecodificada = abre_escrita_arquivo(argO);
+
+            lista_chave = cria_lista_chave();
+            valores_livro_cifra(lista_chave, LivroCifra);;
             decodifica_arquivo(lista_chave, MensagemCodificada, MensagemDecodificada);
 
             fclose(LivroCifra);
@@ -133,7 +105,8 @@ int main(int argc, char **argv){
             fclose(MensagemDecodificada);
         }
     }
+
     destroi_lista_chave(lista_chave);
-	
+    
     return 0; 
 }  
