256163cfbca8466b529566e4917e9af7c2cbfea9
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-04-27 20:42:30 -0300

    Create lib_trata_arquivo.h

---
+++ lib_trata_arquivo.h
@@ -0,0 +1,10 @@
+#ifndef lib_trata_arquivo
+#define lib_trata_arquivo
+
+int arquivo_existe(char *arg);
+
+FILE* abre_leitura_arquivo(char* arg);
+
+FILE* abre_escrita_arquivo(char* arg);
+
+#endif /* lib_trata_arquivo */
