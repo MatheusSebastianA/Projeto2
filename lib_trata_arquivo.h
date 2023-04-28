3f05d269f503456d9efad020b85ddad529500156
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-04-28 13:55:07 -0300

    Update lib_trata_arquivo.h

--- lib_trata_arquivo.h
+++ lib_trata_arquivo.h
@@ -3,6 +3,8 @@
 
 int arquivo_existe(char *arg);
 
+void quantidade_arquivo_passado(int qnt_arquivos[], char *argB,  char *argM, char *argO, char *argC, char *argI);
+
 FILE* abre_leitura_arquivo(char* arg);
 
 FILE* abre_escrita_arquivo(char* arg);
