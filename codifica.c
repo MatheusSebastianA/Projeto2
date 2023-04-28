ecdd3def4352b509b742e283301084b926716fa8
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-04-28 11:08:45 -0300

    Update codifica.c

--- codifica.c
+++ codifica.c
@@ -42,12 +42,10 @@ int codifica_caractere(lista_c_t *l, char c, char *c_cod){
             }
 
             if (cont > 1){ /* Se existir mais de uma posição,será sorteado um valor entre 0 e a quantidade de posições */
-                cont = rand() % cont;
+                cont = rand() % (cont-1);
                 aux_ini_pos = aux->lista_pos->ini;
                 for (i = 0; i < cont; i++){ /* Será acessado a posição na posição aleatoriamente sorteada */        
-                    if(aux_ini_pos->prox != NULL){
                         aux_ini_pos = aux_ini_pos->prox;
-                    }
                 }
             }
             tam = strlen(aux_ini_pos->pos);
