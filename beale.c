dc0aef78a171f8eaca8ba8ff6b23f9303d6e48d8
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-04-28 13:32:12 -0300

    Trata a quantidade de argumentos iguais, verifica se foram passados todos os argumentos necessários

--- beale.c
+++ beale.c
@@ -12,7 +12,7 @@
 
 int main(int argc, char **argv){
     srand(time(NULL));
-    int option, casoE = 0, casoD = 0;
+    int option, casoE = 0, casoD = 0, qnt_arquivos[5] = {0};
     char *argB = NULL, *argM = NULL, *argO = NULL, *argC = NULL, *argI = NULL;
     FILE *LivroCifra = NULL, *MensagemOriginal = NULL, *ArquivoDeChaves = NULL, *MensagemDecodificada = NULL, *MensagemCodificada = NULL;
     lista_c_t *lista_chave;
@@ -28,53 +28,65 @@ int main(int argc, char **argv){
                 break;
             case 'b':
                 argB = optarg;
+                qnt_arquivos[0] += 1;
                 break;
             case 'm':
                 argM = optarg;
+                qnt_arquivos[1] += 1;
                 break;
             case 'o':
                 argO = optarg;
+                qnt_arquivos[2] += 1;
                 break;
             case 'c':
                 argC = optarg;
+                qnt_arquivos[3] += 1;
                 break;
             case 'i':
                 argI = optarg;
+                qnt_arquivos[4] += 1;
                 break;
             default:
                 printf ("O argumento passado não é válido\n");
 	            return (1);
       }   
 
-    if(casoE + casoD != 1){
+
+
+    if(casoE + casoD != 1){ /*verifica se foi passado mais de um -e ou -d*/
         printf("A quantidade de argumentos '-e' ou '-d' é inválida\n");
         return 1;
     }
 
-    if(casoE == 1){
+    if(casoE == 1 && argB != NULL && argM != NULL && argO != NULL){ /* verifica se foi estamos */
+        quantidade_arquivo_passado(qnt_arquivos, argB, argM, argO, argC, argI);
+
+        if((arquivo_existe(argC)))
+            return 1;
 
         LivroCifra = abre_leitura_arquivo(argB);
         MensagemOriginal = abre_leitura_arquivo(argM);
         MensagemCodificada = abre_escrita_arquivo(argO);
+        ArquivoDeChaves = abre_escrita_arquivo(argC);
 
         lista_chave = cria_lista_chave();
         valores_livro_cifra(lista_chave, LivroCifra);
         codifica_arquivo(lista_chave, MensagemOriginal, MensagemCodificada);
+        imprime_lista_chave_arq(lista_chave, ArquivoDeChaves);
 
-        if(!(arquivo_existe(argC))){
-            ArquivoDeChaves = abre_escrita_arquivo(argC);
-            imprime_lista_chave_arq(lista_chave, ArquivoDeChaves);
-            fclose(ArquivoDeChaves);
-        }
-
+        fclose(ArquivoDeChaves);
         fclose(LivroCifra);
         fclose(MensagemOriginal);
         fclose(MensagemCodificada);
     }
     
     else{
-        if(argC != NULL){
+        if(argC != NULL && argI != NULL && argO != NULL){
+            quantidade_arquivo_passado(qnt_arquivos, argB, argM, argO, argC, argI);   
 
+            if(arquivo_existe(argO))
+                return 1;
+            
             ArquivoDeChaves = abre_leitura_arquivo(argC);
             MensagemCodificada = abre_leitura_arquivo(argI);
             MensagemDecodificada = abre_escrita_arquivo(argO);
@@ -89,21 +101,33 @@ int main(int argc, char **argv){
         }
 
         else{
-            if(arquivo_existe(argO))
-                return 1;
+            if(argB != NULL && argI != NULL && argO != NULL){
+                quantidade_arquivo_passado(qnt_arquivos, argB, argM, argO, argC, argI); 
+                
+                if(arquivo_existe(argO))
+                    return 1;
 
-            LivroCifra = abre_leitura_arquivo(argB);
-            MensagemCodificada = abre_leitura_arquivo(argI);
-            MensagemDecodificada = abre_escrita_arquivo(argO);
+                LivroCifra = abre_leitura_arquivo(argB);
+                MensagemCodificada = abre_leitura_arquivo(argI);
+                MensagemDecodificada = abre_escrita_arquivo(argO);
 
-            lista_chave = cria_lista_chave();
-            valores_livro_cifra(lista_chave, LivroCifra);;
-            decodifica_arquivo(lista_chave, MensagemCodificada, MensagemDecodificada);
+                lista_chave = cria_lista_chave();
+                valores_livro_cifra(lista_chave, LivroCifra);;
+                decodifica_arquivo(lista_chave, MensagemCodificada, MensagemDecodificada);
 
-            fclose(LivroCifra);
-            fclose(MensagemCodificada);
-            fclose(MensagemDecodificada);
+                fclose(LivroCifra);
+                fclose(MensagemCodificada);
+                fclose(MensagemDecodificada);
+
+            }
+
+            else{
+                printf("A quantidade de argumentos passados é inválida\n");
+                return 0;
         }
+        }
+
+        
     }
 
     destroi_lista_chave(lista_chave);
